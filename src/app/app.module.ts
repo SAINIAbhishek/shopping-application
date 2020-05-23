bb6d9278ae461f4b10f3b6ba3f34f3b5562f0f35
abhisheksaini <saini.abhishek@hotmail.com>
2020-05-23 16:30:46 +0200

    restructured.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { RecipesComponent } from './modules/recipes/recipes.component';
+import { RecipeListComponent } from './modules/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './modules/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './modules/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './modules/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './modules/shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from "@angular/forms";
-import { DropdownDirective } from "./shared/dropdown.directive";
+import { DropdownDirective } from "./directives/dropdown.directive";
 
 @NgModule({
   declarations: [
