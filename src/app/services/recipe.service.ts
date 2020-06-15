dec5dd3476da78f228353ede50aa970e6bd45617
abhisheksaini <saini.abhishek@hotmail.com>
2020-06-15 23:09:02 +0200

    recipe edit template drive in progress...

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -2,9 +2,12 @@ import {Injectable} from "@angular/core";
 import {Recipe} from "../models/recipe.model";
 import {Ingredient} from "../models/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable({providedIn: "root"})
 export class RecipeService {
+
+	recipesChanged = new Subject<Recipe[]>();
 
 	constructor(private slService: ShoppingListService) {}
 
@@ -32,6 +35,21 @@ export class RecipeService {
 
 	addIngredientsToShoppingList(ingredients: Ingredient[]) {
 		this.slService.addIngredients(ingredients);
+	}
+
+	addRecipe(recipe: Recipe) {
+		this.recipes.push(recipe);
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	updateRecipe(index: number, newRecipe: Recipe) {
+		this.recipes[index] = newRecipe;
+		this.recipesChanged.next(this.recipes.slice());
+	}
+
+	deleteRecipe(index: number) {
+		this.recipes.splice(index, 1);
+		this.recipesChanged.next(this.recipes.slice());
 	}
 
 }
